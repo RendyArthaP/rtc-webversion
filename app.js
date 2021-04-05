1e8b575e5eec87d439cba096ffa7a6075c62cd77
Rendy Artha <rendyartha312@gmail.com>
2021-04-05 20:47:12 +0700

    socket-connection

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const server = require('http').createServer(app)
 const cors = require('cors')
 const routes = require('./routes')
 const PORT = process.env.PORT || 3000
+
 const io = require('socket.io')(server, {
   cors: {
     origin: "*",
@@ -12,6 +13,22 @@ const io = require('socket.io')(server, {
 
 app.use(cors())
 app.use(routes)
+
+io.on('connection', (socket) => {
+  socket.emit('me', socket.id);
+
+  socket.on('disconnect', () => {
+    socket.broadcast.emit('callended');
+  })
+
+  socket.on('calluser', ({ userToCall, signalData, from, name}) => {
+    io.to(userToCall).emit('calluser', { signal: signalData, from, name})
+  })
+
+  socket.on('answercall', (data) => {
+    io.to(data.to).emit("callacepted", data.signal)
+  })
+})
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`)
