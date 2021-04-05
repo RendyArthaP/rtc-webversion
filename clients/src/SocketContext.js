9b01dc09f10d226e78653c3580baa86d2b230923
Rendy Artha <rendyartha312@gmail.com>
2021-04-05 21:57:40 +0700

    setup useeffect socket-io

--- clients/src/SocketContext.js
+++ clients/src/SocketContext.js
@@ -12,5 +12,45 @@ const SocketContext = createContext();
 const socket = io('http://localhost:5000')
 
 const ContextProvider = ({ children }) => {
-  
+  const [stream, setStream] = useState(null)
+  const [me, setMe] = useState('')
+  const [call, setCall] = useState({});
+  const [callAccepted, setCallAccepted] = useState(false);
+  const [callEnded, setCallEnded] = useState(false);
+
+  const myVideo = useRef()
+
+  useEffect(() => {
+    navigator.mediaDevices.getUserMedia({
+      video: true,
+      audio: true
+    })
+      .then((currentStream) => {
+        setStream(currentStream);
+        myVideo.current.srcObject = currentStream
+      })
+    
+    socket.on('me', (id) => setMe(id))
+
+    socket.on('calluser', ({ from, name: callerName, signal}) => {
+      setCall({
+        isRecivedCall: true, 
+        from, 
+        name: callerName, 
+        signal
+      })
+    })
+  }, [])
+
+  const answerCall = () => {
+
+  }
+
+  const callUser = () => {
+
+  }
+
+  const leaveCall = () => {
+
+  }
 }
