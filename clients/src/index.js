cd3c85852488e50d380346d0e3d3c376715a65bb
Rendy Artha <rendyartha312@gmail.com>
2021-04-05 22:30:33 +0700

    setup context provider

--- clients/src/index.js
+++ clients/src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { ContextProvider } from './SocketContext'
 import App from './App';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ContextProvider>
     <App />
-  </React.StrictMode>,
+  </ContextProvider>,
   document.getElementById('root')
 );
 
